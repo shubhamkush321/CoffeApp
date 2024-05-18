2a6c1facb80ab823143396bb9bafd87b482f4e81
shubham <142429585+shubhamkush321@users.noreply.github.com>
2024-05-18 12:08:26 +0530

    Android App

---
+++ src/screens/CardScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CardScreen = () => {
+  return (
+    <View>
+      <Text>CardScreen</Text>
+    </View>
+  )
+}
+
+export default CardScreen
+
+const styles = StyleSheet.create({})
