d8825efa45d57dec0ae3f5f5eb3b7487e3321d39
shubham <142429585+shubhamkush321@users.noreply.github.com>
2024-05-19 08:33:05 +0530

    Merged changes from remote main branch

---
+++ src/components/CoffeeCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CoffeeCard = () => {
+  return (
+    <View>
+      <Text>CoffeeCard</Text>
+    </View>
+  )
+}
+
+export default CoffeeCard
+
+const styles = StyleSheet.create({})
